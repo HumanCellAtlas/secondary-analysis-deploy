7e83f04721e1a81bbf0d2fccc70d5f2e594e8fad
rhiananthony <ranthony@broadinstitute.org>
2019-03-01 14:34:33 -0500

    Reverting changes to generate certs command and flags in config files to gen certs

--- gitlab/generate_certs.sh
+++ gitlab/generate_certs.sh
@@ -26,7 +26,7 @@ export VAULT_TOKEN="$(cat ${VAULT_WRITE_TOKEN_PATH})"
 if [ -f "${WORK_DIR}/certs/letsencrypt/archive/${DOMAIN}/fullchain1.pem" ];
 then
     export FULLCHAIN_VAULT_DIR="${WORK_DIR}/certs/letsencrypt/archive/${DOMAIN}/fullchain1.pem"
-    echo "Writing fullchain to vault at ${FULLCHAIN_VAULT_DIR}"
+    echo "Writing fullchain to vault at secret/dsde/mint/${ENVIRONMENT}/lira/fullchain.pem"
     vault write secret/dsde/mint/${ENVIRONMENT}/lira/fullchain.pem value=@"${FULLCHAIN_VAULT_DIR}"
 else
     echo "Fullchain file doesn't exist. Skipping..."
@@ -35,7 +35,7 @@ fi
 if [ -f "${WORK_DIR}/certs/letsencrypt/archive/${DOMAIN}/privkey1.pem" ];
 then
     export PRIVKEY_VAULT_DIR="${WORK_DIR}/certs/letsencrypt/archive/${DOMAIN}/privkey1.pem"
-    echo "Writing privkey to vault at ${PRIVKEY_VAULT_DIR}"
+    echo "Writing privkey to vault at secret/dsde/mint/${ENVIRONMENT}/lira/privkey.pem"
     vault write secret/dsde/mint/${ENVIRONMENT}/lira/privkey.pem value=@"${PRIVKEY_VAULT_DIR}"
 else
     echo "Private key file doesn't exist. Skipping..."
@@ -44,7 +44,7 @@ fi
 if [ -f "${WORK_DIR}/certs/letsencrypt/archive/${DOMAIN}/chain1.pem" ];
 then
     export CHAIN_VAULT_DIR="${WORK_DIR}/certs/letsencrypt/archive/${DOMAIN}/chain1.pem"
-    echo "Writing chain to vault at ${CHAIN_VAULT_DIR}"
+    echo "Writing chain to vault at secret/dsde/mint/${ENVIRONMENT}/lira/chain.pem"
     vault write secret/dsde/mint/${ENVIRONMENT}/lira/chain.pem value=@"${CHAIN_VAULT_DIR}"
 else
     echo "Chain file doesn't exist. Skipping..."
@@ -53,7 +53,7 @@ fi
 if [ -f "${WORK_DIR}/certs/letsencrypt/archive/${DOMAIN}/cert1.pem" ];
 then
     export CERT_VAULT_DIR="${WORK_DIR}/certs/letsencrypt/archive/${DOMAIN}/cert1.pem"
-    echo "Writing cert to vault at ${CERT_VAULT_DIR}"
+    echo "Writing cert to vault at secret/dsde/mint/${ENVIRONMENT}/lira/cert.pem"
     vault write secret/dsde/mint/${ENVIRONMENT}/lira/cert.pem value=@"${CERT_VAULT_DIR}"
 else
     echo "Cert file doesn't exist. Skipping..."
@@ -62,4 +62,4 @@ fi
 export VAULT_TOKEN="$(cat ${VAULT_READ_TOKEN_PATH})"
 
 echo "Removing local copies of certs"
-#rm -rf certs
+rm -rf certs
