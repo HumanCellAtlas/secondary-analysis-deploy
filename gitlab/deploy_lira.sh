4fcb8c126369136471751df3eaf371f7dff4e409
rhiananthony <ranthony@broadinstitute.org>
2019-08-05 16:08:51 -0400

    test

--- gitlab/deploy_lira.sh
+++ gitlab/deploy_lira.sh
@@ -50,6 +50,8 @@ kubectl apply -f ${CONFIG_DIR}/lira-service.yaml \
 if [[ "${GENERATE_CERTS}" == "true" ]];
 then
     sh "${DEPLOY_DIR}/generate_certs.sh"
+    echo $?
+    echo "***********************************************************************"
     if [[ $? = 1 ]]; then
         exit 1
     fi
