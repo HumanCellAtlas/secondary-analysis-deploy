063030fe884a49baef42205fc49d369e1f39bf5f
rhiananthony <ranthony@broadinstitute.org>
2020-08-12 15:33:39 -0400

    Commenting out the lira autoscaler since this will not be needed

--- jenkins/deploy_lira.sh
+++ jenkins/deploy_lira.sh
@@ -185,22 +185,22 @@ kubectl apply -f ${CONFIG_DIR}/lira-deployment.yaml \
               --record \
               --namespace "${KUBERNETES_NAMESPACE}"
 
-echo "Generating Lira autoscaler file"
-docker run -i --rm -e LIRA_AUTOSCALER_NAME="${LIRA_AUTOSCALER_NAME}" \
-                   -e LIRA_DEPLOYMENT_NAME= "${LIRA_DEPLOYMENT_NAME}" \
-                   -e LIRA_MIN_REPLICAS="${LIRA_MIN_REPLICAS}" \
-                   -e LIRA_MAX_REPLICAS="${LIRA_MAX_REPLICAS}" \
-                   -e LIRA_PERCENT_TARGET_CPU_USAGE="${LIRA_PERCENT_TARGET_CPU_USAGE}" \
-                   -v "${VAULT_READ_TOKEN_PATH}":/root/.vault-token \
-                   -v "${PWD}":/working \
-                   --privileged \
-                   broadinstitute/dsde-toolbox:ra_rendering \
-                   /usr/local/bin/render-ctmpls.sh \
-                    -k "${DOCKER_CONFIG_DIR}/lira-autoscaler.yaml.ctmpl"
-
-cat "${CONFIG_DIR}/lira-autoscaler.yaml"
-
-echo "Deploying Lira autoscaler"
-kubectl apply -f "${CONFIG_DIR}/lira-autoscaler.yaml" \
-              --record \
-              --namespace "${KUBERNETES_NAMESPACE}"
+#echo "Generating Lira autoscaler file"
+#docker run -i --rm -e LIRA_AUTOSCALER_NAME="${LIRA_AUTOSCALER_NAME}" \
+#                   -e LIRA_DEPLOYMENT_NAME= "${LIRA_DEPLOYMENT_NAME}" \
+#                   -e LIRA_MIN_REPLICAS="${LIRA_MIN_REPLICAS}" \
+#                   -e LIRA_MAX_REPLICAS="${LIRA_MAX_REPLICAS}" \
+#                   -e LIRA_PERCENT_TARGET_CPU_USAGE="${LIRA_PERCENT_TARGET_CPU_USAGE}" \
+#                   -v "${VAULT_READ_TOKEN_PATH}":/root/.vault-token \
+#                   -v "${PWD}":/working \
+#                   --privileged \
+#                   broadinstitute/dsde-toolbox:ra_rendering \
+#                   /usr/local/bin/render-ctmpls.sh \
+#                    -k "${DOCKER_CONFIG_DIR}/lira-autoscaler.yaml.ctmpl"
+#
+#cat "${CONFIG_DIR}/lira-autoscaler.yaml"
+#
+#echo "Deploying Lira autoscaler"
+#kubectl apply -f "${CONFIG_DIR}/lira-autoscaler.yaml" \
+#              --record \
+#              --namespace "${KUBERNETES_NAMESPACE}"
